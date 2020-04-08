63162e62db7c0ed0227283c1ed993b1abd988045
fuku fuku <shofuku57@gmail.com>
2020-04-08 10:05:18 +0000

    試し

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,17 +1,34 @@
 class ImageUploader < CarrierWave::Uploader::Base
-  include Cloudinary::CarrierWave if Rails.env.production?
-  include CarrierWave::RMagick
-  process resize_to_fill: [150, 150, "Center"]
-  
-  storage :file
+  if Rails.env.production?
+    include Cloudinary::CarrierWave
+    process :convert => 'png'
+    process :tags => ['image']
+
+    version :standard do
+    process :resize_to_fill => [100, 150, :north]
+    end
+
+    version :thumbnail do
+    process :resize_to_fit => [150, 150, "Center"]
+    end
+
+    def public_id
+      return model.short_name
+    end
+
+  else
+    include CarrierWave::RMagick
+    process resize_to_fill: [128,128]
+    storage :file
     # アップロードファイルの保存先ディレクトリは上書き可能
     # 下記はデフォルトの保存先
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
+    def store_dir
+      "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    end
     # アップロード可能な拡張子のリスト
-  def extension_white_list
-    %w(jpg jpeg gif png)
+    def extension_white_list
+      %w(jpg jpeg gif png)
+    end
   end
 end 
 # Include RMagick or MiniMagick support:
