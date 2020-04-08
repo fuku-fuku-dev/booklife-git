558a862e42abb22c6bb892e1dbb833bb33728cb5
fuku fuku <shofuku57@gmail.com>
2020-04-08 10:33:34 +0000

    試し

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,11 +5,13 @@ class ImageUploader < CarrierWave::Uploader::Base
     process :tags => ['image']
 
     version :standard do
-    process :resize_to_fill => [100, 150, :north]
+      process :eager => true
+      process :resize_to_fill => [100, 150, :north]
     end
 
     version :thumbnail do
-    process :resize_to_fit => [150, 150, "Center"]
+      eager
+      process :resize_to_fit => [150, 150, "Center"]
     end
 
     def public_id
